ed34672d87234c860144bac265eb0a07e22b2b47
pavel-khudzik <pavel_khudzik@epam.com>
2026-03-05 16:02:59 +0000

    django update

--- octofit-tracker/backend/octofit_tracker/api_urls.py
+++ octofit-tracker/backend/octofit_tracker/api_urls.py
@@ -1,6 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+
 from .views import UserViewSet, TeamViewSet, ActivityViewSet, WorkoutViewSet, LeaderboardViewSet, api_root
+import os
+from django.http import JsonResponse
+from django.urls import reverse
+from rest_framework.decorators import api_view
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet)
@@ -9,7 +14,22 @@ router.register(r'activities', ActivityViewSet)
 router.register(r'workouts', WorkoutViewSet)
 router.register(r'leaderboard', LeaderboardViewSet)
 
+@api_view(['GET'])
+def custom_api_root(request, format=None):
+    codespace_name = os.environ.get('CODESPACE_NAME')
+    if codespace_name:
+        base_url = f'https://{codespace_name}-8000.app.github.dev/api/'
+    else:
+        base_url = request.build_absolute_uri('/api/')
+    return JsonResponse({
+        'users': base_url + 'users/',
+        'teams': base_url + 'teams/',
+        'activities': base_url + 'activities/',
+        'workouts': base_url + 'workouts/',
+        'leaderboard': base_url + 'leaderboard/',
+    })
+
 urlpatterns = [
-    path('', api_root, name='api-root'),
+    path('', custom_api_root, name='api-root'),
     path('', include(router.urls)),
 ]
